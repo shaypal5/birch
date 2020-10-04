728eb4513ebad64da110ca1d8627661904542f58
Shay Palachy <shaypal5@gmail.com>
2020-10-04 16:17:08 +0300

    now casters is an explicit submodule

--- birch/__init__.py
+++ birch/__init__.py
@@ -2,6 +2,7 @@
 
 from .core import Birch  # noqa: F401
 import birch.exceptions as exceptions  # noqa: F401
+import birch.casters as casters  # noqa: F401
 
 from ._version import get_versions
 __version__ = get_versions()['version']
